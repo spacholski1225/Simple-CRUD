1735bc1cabad0416412c024a6993ce9fcc20507d
spacholski1225 <spacholski1225@gmail.com>
2021-01-25 16:07:30 +0100

    created asp core mvc program

---
+++ eIndex/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eIndex.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
