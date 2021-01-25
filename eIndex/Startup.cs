25cc4877d1c85aefe2725aeaded15a287f108e2d
spacholski1225 <spacholski1225@gmail.com>
2021-01-25 16:29:05 +0100

    added simple crud with database

--- eIndex/Startup.cs
+++ eIndex/Startup.cs
@@ -1,6 +1,8 @@
+using eIndex.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,11 @@ namespace eIndex
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<SchoolContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("eIndexDbConnection")));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
